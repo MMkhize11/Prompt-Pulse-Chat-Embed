8af012aa8855c1c49ea2750c73eaa4514c33d080
Mpumelelo Mkhize <mmkhize@argility.com>
2024-02-10 22:40:43 +0200

    [fix]: clean up

--- src/window.ts
+++ src/window.ts
@@ -9,13 +9,13 @@ type BotProps = {
 };
 
 export const initFull = (props: BotProps & { id?: string }) => {
-  const fullElement = props.id ? document.getElementById(props.id) : document.querySelector('flowise-fullchatbot');
-  if (!fullElement) throw new Error('<flowise-fullchatbot> element not found.');
+  const fullElement = props.id ? document.getElementById(props.id) : document.querySelector('prompt-pulse-fullchatbot');
+  if (!fullElement) throw new Error('<prompt-pulse-fullchatbot> element not found.');
   Object.assign(fullElement, props);
 };
 
 export const init = (props: BotProps) => {
-  const element = document.createElement('flowise-chatbot');
+  const element = document.createElement('prompt-pulse-chatbot');
   Object.assign(element, props);
   document.body.appendChild(element);
 };
